384050ebe898be67cf585c02d2f944651ed88da0
Valentin Bersier <valentin.bersier@mm-welding.com>
2020-05-25 22:23:12 +0200

    Trying to format

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 };
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   pwa: {
-    name: 'Coffee Scale',
-    themeColor: '#7C6D68',
-    msTileColor: '#DD545C',
+    name: "Coffee Scale",
+    themeColor: "#7C6D68",
+    msTileColor: "#DD545C",
     manifestOptions: {
-      background_color: '#6C5662',
-    },
+      background_color: "#6C5662"
+    }
   },
+
+  lintOnSave: false
 };
