6eb8b8ad2caef0c25ef80a6f23420dc81009213f
Valentin Bersier <valentin.bersier@mm-welding.com>
2020-05-26 18:43:31 +0200

    Chart work

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,68 @@
 <template>
   <b-container fluid="lg" id="app">
     <h1>Brew</h1>
+    <chart :chart-data="datacollection" :styles="chartStyles"></chart>
   </b-container>
 </template>
 
 <script>
+import Chart from "./components/Chart.vue";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    Chart
+  },
+  data() {
+    return {
+      datacollection: null
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        // labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#eee",
+            fill: true,
+            lineTension: 0,
+            data: [
+              { x: 0, y: this.getRandomInt() },
+              { x: 1, y: this.getRandomInt() },
+              { x: 3, y: this.getRandomInt() }
+            ]
+          },
+          {
+            label: "Data Two",
+            backgroundColor: "#528078",
+            borderColor: "#528078",
+            fill: false,
+            lineTension: 0,
+            data: [
+              { x: 0, y: this.getRandomInt() },
+              { x: 1, y: this.getRandomInt() }
+            ]
+          }
+        ]
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    }
+  },
+  computed: {
+    chartStyles() {
+      return {
+        height: "calc(100vh - 4rem)",
+        position: "relative"
+      };
+    }
+  }
 };
 </script>
 
---
+++ src/components/Chart.vue
@@ -0,0 +1,34 @@
+<script>
+import { Line, mixins } from "vue-chartjs";
+
+const { reactiveProp } = mixins;
+
+export default {
+  name: "Chart",
+  extends: Line,
+  mixins: [reactiveProp],
+  data() {
+    return {
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
