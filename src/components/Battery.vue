d28f6e4ad0938a56a42ff05e73a815aabbb8401b
Valentin Bersier <vbersier@gmail.com>
2020-06-07 08:47:35 +0200

    Added battery indicator for different levels

--- src/components/Battery.vue
+++ src/components/Battery.vue
@@ -1,7 +1,12 @@
 <template>
   <div v-if="batteryLevel" id="battery">
     <span>{{ batteryLevel }}%&nbsp;</span>
-    <b-icon-battery-full></b-icon-battery-full>
+    <b-icon-battery-full v-if="batteryLevel >= 60" :style="{ verticalAlign: 'middle' }"></b-icon-battery-full>
+    <b-icon-battery-half
+      v-if="batteryLevel < 60 && batteryLevel >= 30"
+      :style="{ verticalAlign: 'middle' }"
+    ></b-icon-battery-half>
+    <b-icon-battery v-if="batteryLevel < 30" variant="danger" :style="{ verticalAlign: 'middle' }"></b-icon-battery>
   </div>
 </template>
 
@@ -19,8 +24,8 @@ export default {
 <style scoped lang="scss">
 #battery {
   position: absolute;
-  right: 0;
-  bottom: 0;
+  right: 2.5rem;
+  bottom: 0.8rem;
   font-size: 1.3rem;
 
   span {
