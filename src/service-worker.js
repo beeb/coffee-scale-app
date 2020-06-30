5947b15fc3c01fc3c578b2aaa1bf6dbaf2e667ff
Valentin Bersier <vbersier@gmail.com>
2020-06-30 07:43:16 +0200

    Bumped version number

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,7 +3,7 @@
 
 workbox.core.setCacheNameDetails({ prefix: 'coffee-scale-app' })
 //Change this value every time before you build
-const LATEST_VERSION = '1.0.7'
+const LATEST_VERSION = '1.0.8'
 self.addEventListener('activate', () => {
   console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff')
   if (caches) {
