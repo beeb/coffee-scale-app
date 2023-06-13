abdf06d89bd1c37e8df97b36290cc085d80500b8
beeb <vbersier@gmail.com>
2023-06-13 21:26:15 +0200

    feat: drawer for form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,24 @@
   import Form from './Form.svelte'
   import Title from './Title.svelte'
   import { Toaster } from 'svelte-french-toast'
+  import Settings from 'virtual:icons/mingcute/settings-1-line'
 </script>
 
-<div class="w-full h-full max-w-7xl mx-auto relative">
-  <Chart />
-  <div class="absolute max-w-7xl inset-0 z-10">
-    <Title />
+<div class="w-full h-full max-w-7xl mx-auto relative drawer drawer-end">
+  <input id="form-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content">
+    <Chart />
+    <div class="absolute max-w-7xl inset-0">
+      <Title />
+      <div class="absolute right-10 top-10">
+        <label for="form-drawer" class="btn btn-neutral drawer-button btn-square" aria-label="Open drawer">
+          <Settings class="h-6 w-6" />
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="drawer-side">
+    <label for="form-drawer" class="drawer-overlay" />
     <Form />
   </div>
 </div>
--- src/routes/Form.svelte
+++ src/routes/Form.svelte
@@ -3,13 +3,17 @@
   import InputPreinfusion from './InputPreinfusion.svelte'
   import InputRatio from './InputRatio.svelte'
   import InputTotalTime from './InputTotalTime.svelte'
+  import ArrowRight from 'virtual:icons/mingcute/arrow-right-line'
 </script>
 
-<div class="absolute right-14 bottom-20">
-  <div class="flex flex-col">
-    <InputCoffeeWeight />
-    <InputRatio />
-    <InputPreinfusion />
-    <InputTotalTime />
+<div class="flex flex-col bg-base-100 p-4 h-full">
+  <div class="absolute top-2 right-2">
+    <label for="form-drawer" class="btn btn-sm btn-ghost btn-square" aria-label="Close drawer">
+      <ArrowRight class="h-6 w-6" />
+    </label>
   </div>
+  <InputCoffeeWeight />
+  <InputRatio />
+  <InputPreinfusion />
+  <InputTotalTime />
 </div>
