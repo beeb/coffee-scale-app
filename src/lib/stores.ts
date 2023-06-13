856292a3a19d741f92f47d46ad4c4730da29e692
beeb <vbersier@gmail.com>
2023-06-13 21:56:39 +0200

    feat: persistence

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,4 @@
+import { writable as persistentWritable } from '@macfja/svelte-persistent-store'
 import { type Writable, derived, writable } from 'svelte/store'
 
 export const btEnabled = writable(false)
@@ -5,10 +6,10 @@ export const btConnected = writable(false)
 export const recording = writable(false)
 export const startTimeMs = writable(0)
 
-export const coffeeWeight = writable(18.0)
-export const targetRatio = writable(2.0)
-export const preInfusion = writable(5.0)
-export const totalTime = writable(30.0)
+export const coffeeWeight = persistentWritable('li.beeb.coffee.v2.coffeeWeight', 18.0)
+export const targetRatio = persistentWritable('li.beeb.coffee.v2.targetRatio', 2.0)
+export const preInfusion = persistentWritable('li.beeb.coffee.v2.preInfusion', 5.0)
+export const totalTime = persistentWritable('li.beeb.coffee.v2.totalTime', 30.0)
 
 export const currentWeight = writable(0.0)
 export const batteryLevel: Writable<null | number> = writable(null)
