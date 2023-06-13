b5922f0d65e3f171da47bdac2cba85ffbe020180
beeb <vbersier@gmail.com>
2023-06-13 08:09:13 +0200

    feat: favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<svelte:head>
+  <title>Coffee Scale</title>
+</svelte:head>
