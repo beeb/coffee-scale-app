42bc67c056af17ac3345bdf96d582626477eae1e
beeb <vbersier@gmail.com>
2023-06-13 17:05:11 +0200

    feat: css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,11 @@
   import '../app.css'
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="btn btn-primary">Test</button>
+<div class="w-full h-full border-2 border-red-500">
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  <button class="btn btn-primary">Test</button>
+</div>
 
 <svelte:head>
   <title>Coffee Scale</title>
