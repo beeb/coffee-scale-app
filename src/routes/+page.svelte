946382d0a290d76d04d4b9466ec6ebaa77fec2ab
beeb <vbersier@gmail.com>
2023-06-13 17:09:57 +0200

    feat: add input for testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <div class="w-full h-full border-2 border-red-500">
   <h1>Welcome to SvelteKit</h1>
   <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <button class="btn btn-primary">Test</button>
+  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
 </div>
 
 <svelte:head>
