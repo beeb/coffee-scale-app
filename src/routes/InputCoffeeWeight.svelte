3625fc570e34c1fadb34a167273615b5b2edbfcd
beeb <vbersier@gmail.com>
2023-06-13 21:06:56 +0200

    fix: button background

--- src/routes/InputCoffeeWeight.svelte
+++ src/routes/InputCoffeeWeight.svelte
@@ -7,14 +7,14 @@
     <span class="label-text text-xs md:text-sm">Coffee weight</span>
   </label>
   <div class="join">
-    <button type="button" class="btn btn-primary btn-outline join-item btn-sm">Read</button>
+    <button type="button" class="btn btn-primary join-item btn-sm">Read</button>
     <input
       id="coffee-weight"
       type="number"
       bind:value={$coffeeWeight}
       min="0"
       step="0.5"
-      class="input input-bordered input-sm w-full max-w-xs join-item border-l-primary"
+      class="input input-bordered input-sm w-full max-w-xs join-item"
     />
     <div class="join-item bg-base-200 px-3 text-sm inline-flex items-center">g</div>
   </div>
