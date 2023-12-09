2719107d9001b2ad89a1ac6bac0c91c32a8914e6
beeb <703631+beeb@users.noreply.github.com>
2023-12-09 17:48:02 +0100

    wip: bluetooth stuff

--- coffee-scale-firmware/src/main.rs
+++ coffee-scale-firmware/src/main.rs
@@ -3,21 +3,26 @@
 
 // cargo espflash flash --release --bootloader C:\tb\target\xtensa-esp32-espidf\release\build\esp-idf-sys-b70e5b0d2fa5cc23\out\build\bootloader\bootloader.bin
 
-use embedded_graphics::{
-    image::Image,
-    mono_font::{ascii::FONT_6X10, MonoTextStyle},
-    pixelcolor::{BinaryColor, Gray4},
-    prelude::*,
-    text::Text,
+use bleps::{
+    ad_structure::{
+        create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
+    },
+    att::Uuid,
+    attribute_server::{AttributeServer, NotificationData, WorkResult},
+    gatt, Ble, HciConnector,
 };
 use esp_backtrace as _;
-use esp_wifi::{initialize, EspWifiInitFor};
+use esp_wifi::{ble::controller::BleConnector, initialize, EspWifiInitFor};
 use hal::{
-    clock::ClockControl, gpio::IO, i2c::I2C, peripherals::Peripherals, prelude::*,
-    timer::TimerGroup, Delay, Rng,
+    clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay, Rng,
 };
-use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
-use tinybmp::Bmp;
+
+// org.bluetooth.characteristic.gap.appearance.xml
+const ADV_APPEARANCE_TYPE: u8 = 0x19;
+const APPEARANCE_GENERIC_WEIGHT_SCALE: [u8; 2] = 0x032u16.to_le_bytes();
+
+const AUTOMATION_IO_UUID: Uuid = Uuid::Uuid16(0x1815);
+const BATTERY_UUID: Uuid = Uuid::Uuid16(0x180F);
 
 #[entry]
 fn main() -> ! {
@@ -33,17 +38,20 @@ fn main() -> ! {
     esp_println::logger::init_logger_from_env();
     log::info!("Logging started");
 
-    let _init = initialize(
+    let Ok(init) = initialize(
         EspWifiInitFor::Ble,
         timer,
         Rng::new(peripherals.RNG),
         system.radio_clock_control,
         &clocks,
-    )
-    .unwrap();
+    ) else {
+        panic!("Failed to initialize esp-wifi");
+    };
     log::info!("BLE initialized");
 
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut bluetooth = peripherals.BT;
+
+    /*let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
     let i2c = I2C::new(
         peripherals.I2C0,
@@ -51,44 +59,101 @@ fn main() -> ! {
         io.pins.gpio22,
         100u32.kHz(),
         &clocks,
-    );
-
-    let mut display = ssd1327_i2c::SSD1327I2C::with_wh(i2c, 128, 96);
-
-    display.init();
-
-    let style = MonoTextStyle::new(&FONT_6X10, Gray4::WHITE);
-
-    Text::new("Hello rust!", Point::new(10, 10), style)
-        .draw(&mut display)
-        .unwrap();
-
-    /* let interface = I2CDisplayInterface::new(i2c);
-
-    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-        .into_buffered_graphics_mode();
-
-    display.init().unwrap(); */
-
-    log::info!("Display initialized");
-
-    /* let bmp: Bmp<Gray4> = Bmp::from_slice(include_bytes!("../assets/hex.bmp")).unwrap();
-    let image = Image::new(&bmp, Point::new(32, 32));
-    image.draw(&mut display).unwrap();
-    log::info!("Image should be displayed"); */
+    ); */
 
     loop {
-        /*     image.draw(&mut display).unwrap();
-        log::info!("Image should be displayed"); */
-        delay.delay_ms(500u32);
+        let connector = BleConnector::new(&init, &mut bluetooth);
+        let hci = HciConnector::new(connector, esp_wifi::current_millis);
+        let mut ble = Ble::new(&hci);
+        match ble.init() {
+            Ok(_) => log::info!("BLE initialized"),
+            Err(e) => panic!("Failed to initialize BLE: {:?}", e),
+        }
+        match ble.cmd_set_le_advertising_parameters() {
+            Ok(_) => log::info!("BLE advertising parameters set"),
+            Err(e) => panic!("Failed to set advertising parameters: {:?}", e),
+        }
 
-        /* display
-            .send_cmd(ssd1327_i2c::Commands::DisplayModeAllON)
-            .unwrap();
-        delay.delay_ms(1000u32);
-        display
-            .send_cmd(ssd1327_i2c::Commands::DisplayModeAllOFF)
-            .unwrap();
-        delay.delay_ms(1000u32); */
+        let Ok(adv_data) = create_advertising_data(&[
+            AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+            AdStructure::ServiceUuids16(&[AUTOMATION_IO_UUID, BATTERY_UUID]),
+            AdStructure::CompleteLocalName("mpy-coffee"),
+            // appearance
+            AdStructure::Unknown {
+                ty: ADV_APPEARANCE_TYPE,
+                data: &APPEARANCE_GENERIC_WEIGHT_SCALE,
+            },
+        ]) else {
+            panic!("Failed to create advertising data");
+        };
+        match ble.cmd_set_le_advertising_data(adv_data) {
+            Ok(_) => log::info!("BLE advertising data set"),
+            Err(e) => panic!("Failed to set advertising data: {:?}", e),
+        }
+        match ble.cmd_set_le_advertise_enable(true) {
+            Ok(_) => log::info!("BLE advertising enabled"),
+            Err(e) => panic!("Failed to enable advertising: {:?}", e),
+        }
+
+        log::info!("Started advertising");
+
+        let battery_percent: u8 = 50; // 50%
+        let weight_value: i16 = 10 * 100; // 10g
+
+        let mut weight = |_offset: usize, data: &mut [u8]| {
+            data[..2].copy_from_slice(&weight_value.to_be_bytes());
+            2
+        };
+
+        let mut bat = |_offset: usize, data: &mut [u8]| {
+            data[..1].copy_from_slice(&battery_percent.to_be_bytes());
+            1
+        };
+
+        gatt!([
+            service {
+                uuid: "0000180F-0000-1000-8000-00805F9B34FB", // Battery
+                characteristics: [characteristic {
+                    name: "battery",
+                    uuid: "00002A19-0000-1000-8000-00805F9B34FB", // 0x2A19 = Battery Level
+                    read: bat,
+                },],
+            },
+            service {
+                uuid: "00001815-0000-1000-8000-00805F9B34FB", // Automation IO
+                characteristics: [characteristic {
+                    name: "weight",
+                    uuid: "00002A59-0000-1000-8000-00805F9B34FB", // 0x2A59 = analog IO
+                    read: weight,
+                    notify: true,
+                },],
+            }
+        ]);
+
+        let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes);
+        loop {
+            let mut notification = None;
+            let mut buf = [0u8; 1];
+            if let Some(1) = srv.get_characteristic_value(weight_notify_enable_handle, 0, &mut buf)
+            {
+                if buf[0] == 1 {
+                    // notitications enabled
+                    notification = Some(NotificationData::new(
+                        weight_handle,
+                        &weight_value.to_be_bytes(),
+                    ));
+                }
+            }
+            match srv.do_work_with_notification(notification) {
+                Ok(res) => {
+                    if let WorkResult::GotDisconnected = res {
+                        break;
+                    }
+                }
+                Err(err) => {
+                    log::error!("{:?}", err);
+                }
+            }
+        }
     }
 }
