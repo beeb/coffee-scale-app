eb68ae6adbd5f569eff343087d7fcbcc55bc0761
beeb <703631+beeb@users.noreply.github.com>
2023-12-13 22:11:24 +0100

    feat: screen

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,13 +1,26 @@
 use std::time::Duration;
 
 use anyhow::{anyhow, Result};
+use embedded_graphics::{
+    geometry::Point,
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    text::{Baseline, Text},
+    Drawable,
+};
 use esp_idf_svc::{
     hal::{
         adc::ADC1,
         gpio::{Gpio12, Gpio14, Gpio34},
+        i2c,
         peripherals::Peripherals,
+        prelude::*,
     },
     systime::EspSystemTime,
+};
+use ssd1306::{
+    mode::DisplayConfig, rotation::DisplayRotation, size::DisplaySize128x32, I2CDisplayInterface,
+    Ssd1306,
 };
 
 use crate::weight::Scales;
@@ -26,6 +39,26 @@ fn main() -> Result<()> {
     let pins = peripherals.pins;
 
     log::info!("Starting up...");
+
+    let i2c = i2c::I2cDriver::new(
+        peripherals.i2c0,
+        pins.gpio21,
+        pins.gpio22,
+        &i2c::I2cConfig::new().baudrate(400.kHz().into()),
+    )?;
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x32, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+    display.flush().unwrap();
 
     ble::init()?;
     log::info!("BLE initialized");
