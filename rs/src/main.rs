7ac494fd660d58943dfbd0752d39699b78f62422
beeb <703631+beeb@users.noreply.github.com>
2023-12-13 21:58:11 +0100

    refactor: scales struct

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,19 +1,16 @@
-use std::{collections::VecDeque, time::Duration};
+use std::time::Duration;
 
 use anyhow::{anyhow, Result};
 use esp_idf_svc::{
     hal::{
         adc::ADC1,
-        delay::Delay,
         gpio::{Gpio12, Gpio14, Gpio34},
         peripherals::Peripherals,
     },
     systime::EspSystemTime,
-    timer::EspTaskTimerService,
 };
-use loadcell::LoadCell;
 
-use crate::weight::LOADCELL_READY_DELAY;
+use crate::weight::Scales;
 
 mod battery;
 mod ble;
@@ -42,45 +39,20 @@ fn main() -> Result<()> {
         .lock()
         .set_value(&battery_percent.to_be_bytes());
 
-    let mut load_sensor = weight::init_load_sensor::<Gpio14, Gpio12>(pins.gpio14, pins.gpio12)?;
-    let delay = Delay::new_default();
+    let mut scales = Scales::<Gpio12, Gpio14>::new(pins.gpio12, pins.gpio14)?;
 
     if CALIBRATE_MODE {
-        load_sensor.tare(1);
+        // loop indefinitely
+        scales.tare();
         loop {
-            let mut current;
-            let mut average: f32 = 0.0;
-            for n in 1..=10 {
-                while !load_sensor.is_ready() {
-                    delay.delay_us(LOADCELL_READY_DELAY);
-                }
-                current = load_sensor.read() as f32;
-                delay.delay_us(LOADCELL_READY_DELAY * 2);
-                average += (current - average) / (n as f32);
-            }
+            let average = scales.read_average(10);
             log::info!("Weight reading: {:.4}", average);
         }
     }
 
-    // take readings of the loadcell and keep iterating until the weight is stable
-    let mut readings: VecDeque<f32> = VecDeque::with_capacity(10);
-    loop {
-        while !load_sensor.is_ready() {
-            delay.delay_us(LOADCELL_READY_DELAY);
-        }
-        let reading = load_sensor.read_scaled();
-        log::info!("Waiting for stable weight: {:.4}", reading);
-        if readings.len() == 10 {
-            readings.pop_front();
-        }
-        readings.push_back(reading);
-        if readings.len() == 10 && readings.iter().all(|&x| (x - reading).abs() < 0.1) {
-            break;
-        }
-        delay.delay_us(LOADCELL_READY_DELAY * 2);
-    }
-    log::info!("offset: {}", load_sensor.get_offset());
-    load_sensor.tare(10);
+    scales.wait_stable();
+
+    scales.tare();
 
     let mut weight;
 
@@ -88,7 +60,7 @@ fn main() -> Result<()> {
     let mut last_notify = Duration::default();
 
     loop {
-        weight = weight::read_weight(&mut load_sensor, &delay);
+        weight = scales.read_weight();
         log::info!("weight: {weight}");
         let now = system_time.now();
         if now - last_notify > Duration::from_millis(200) {
--- rs/src/weight.rs
+++ rs/src/weight.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use anyhow::Result;
 use esp_idf_svc::hal::{
     delay::Delay,
@@ -14,37 +16,78 @@ pub const LOADCELL_READY_DELAY: u32 = 5000;
 pub type LoadSensor<'a, SckPin, DtPin> =
     HX711<PinDriver<'a, SckPin, Output>, PinDriver<'a, DtPin, Input>, Delay>;
 
-pub fn init_load_sensor<'a, DtPin, SckPin>(
-    data_pin: DtPin,
-    clock_pin: SckPin,
-) -> Result<LoadSensor<'a, SckPin, DtPin>>
+pub struct Scales<'a, SckPin, DtPin>
 where
     DtPin: Peripheral<P = DtPin> + Pin + InputPin,
     SckPin: Peripheral<P = SckPin> + Pin + OutputPin,
 {
-    let hx711_sck = gpio::PinDriver::output(clock_pin)?;
-    let hx711_dt = gpio::PinDriver::input(data_pin)?;
-
-    let delay = Delay::new_default();
-    let mut load_sensor = hx711::HX711::new(hx711_sck, hx711_dt, delay);
-    load_sensor.set_scale(1.57e-4);
-    while !load_sensor.is_ready() {
-        delay.delay_ms(10);
-    }
-    Ok(load_sensor)
+    sensor: LoadSensor<'a, SckPin, DtPin>,
+    delay: Delay,
 }
 
-pub fn read_weight<DtPin, SckPin>(
-    load_sensor: &mut LoadSensor<'_, SckPin, DtPin>,
-    delay: &Delay,
-) -> i16
+impl<'a, SckPin, DtPin> Scales<'a, SckPin, DtPin>
 where
-    DtPin: Pin,
-    SckPin: Pin,
+    DtPin: Peripheral<P = DtPin> + Pin + InputPin,
+    SckPin: Peripheral<P = SckPin> + Pin + OutputPin,
 {
-    while !load_sensor.is_ready() {
-        delay.delay_us(LOADCELL_READY_DELAY);
+    pub fn new(clock_pin: SckPin, data_pin: DtPin) -> Result<Self> {
+        let hx711_sck = gpio::PinDriver::output(clock_pin)?;
+        let hx711_dt = gpio::PinDriver::input(data_pin)?;
+
+        let delay = Delay::new_default();
+        let mut sensor = hx711::HX711::new(hx711_sck, hx711_dt, delay);
+        sensor.set_scale(1.57e-4);
+        while !sensor.is_ready() {
+            delay.delay_ms(10);
+        }
+        Ok(Scales { sensor, delay })
     }
-    let reading = (load_sensor.read_scaled() / 0.05).round() * 0.05; // rounded to 0.05g
-    (reading * 100.) as i16
+
+    pub fn wait_ready(&self) {
+        while !self.sensor.is_ready() {
+            self.delay.delay_us(LOADCELL_READY_DELAY);
+        }
+    }
+
+    pub fn wait_stable(&mut self) {
+        // take readings of the loadcell and keep iterating until the weight is stable
+        let mut readings: VecDeque<f32> = VecDeque::with_capacity(10);
+        loop {
+            while !self.sensor.is_ready() {
+                self.delay.delay_us(LOADCELL_READY_DELAY);
+            }
+            let reading = self.sensor.read_scaled();
+            log::info!("Waiting for stable weight: {:.4}", reading);
+            if readings.len() == 10 {
+                readings.pop_front();
+            }
+            readings.push_back(reading);
+            if readings.len() == 10 && readings.iter().all(|&x| (x - reading).abs() < 0.1) {
+                break;
+            }
+            self.delay.delay_us(LOADCELL_READY_DELAY * 2);
+        }
+    }
+
+    pub fn tare(&mut self) {
+        self.sensor.tare(10);
+    }
+
+    pub fn read_average(&mut self, count: usize) -> f32 {
+        let mut current;
+        let mut average: f32 = 0.0;
+        for n in 1..=count {
+            self.wait_ready();
+            current = self.sensor.read() as f32;
+            self.delay.delay_us(LOADCELL_READY_DELAY * 2);
+            average += (current - average) / (n as f32);
+        }
+        average
+    }
+
+    pub fn read_weight(&mut self) -> i16 {
+        self.wait_ready();
+        let reading = (self.sensor.read_scaled() / 0.05).round() * 0.05; // rounded to 0.05g
+        (reading * 100.) as i16
+    }
 }
