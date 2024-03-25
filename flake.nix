bec42e0ed3a456e5f5708ece7a15b036541fa82b
Valentin B <703631+beeb@users.noreply.github.com>
2024-03-25 22:40:25 +0100

    chore: update and simplify flake (#45)

--- flake.nix
+++ flake.nix
@@ -2,29 +2,25 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, utils, devenv, ... } @ inputs:
+  outputs = { self, nixpkgs, utils, ... } @ inputs:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.outputs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+        };
       in
       {
-        devShell = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            ({ pkgs, ... }: {
-              packages = with pkgs; [
-                nodePackages.pnpm
-              ];
-              languages.javascript = {
-                enable = true;
-                corepack.enable = true;
-              };
-              languages.typescript.enable = true;
-            })
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs-slim
+            nodePackages.pnpm
+            typescript
           ];
+          shellHook = ''
+            set -a; source .env; set+a
+          '';
         };
       });
 }
