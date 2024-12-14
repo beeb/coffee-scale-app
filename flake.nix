14971b84b3828dab35afa233677066fed1cc4ae3
beeb <703631+beeb@users.noreply.github.com>
2024-12-14 11:14:40 +0100

    chore: update dependencies

--- flake.nix
+++ flake.nix
@@ -14,8 +14,8 @@
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            nodejs-slim
-            nodePackages.pnpm
+            nodejs-slim_22
+            corepack_22
             typescript
           ];
         };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
+import daisyui from 'daisyui'
+import themes from 'daisyui/src/theming/themes'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [require('daisyui')],
+	plugins: [daisyui],
 	daisyui: {
 		themes: [
 			{
 				emerald: {
-					...require('daisyui/src/theming/themes').emerald,
+					...themes.emerald,
 					primary: '#42b983',
 					'primary-content': '#ffffff',
 					'--btn-text-case': 'none',
