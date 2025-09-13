7c9c0db7bf7fe5089dcdb78405e75682d7e97b42
beeb <703631+beeb@users.noreply.github.com>
2025-09-13 10:27:31 +0200

    chore(deps): update deps

--- flake.nix
+++ flake.nix
@@ -1,23 +1,30 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            nodejs-slim_22
-            corepack_22
-            typescript
-          ];
-        };
-      });
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              biome
+              nodejs-slim
+              pnpm
+              prettier
+              typescript
+            ];
+          };
+        }
+      );
+    };
 }
